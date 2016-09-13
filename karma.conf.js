5a865f397aed7a8e8a70b10b20b996275972143f
Allan Martins de Paula <allan.mdepaula@gmail.com>
2016-09-12 21:43:17 -0300

    Karma.conf and travis ci file adjustments.

--- karma.conf.js
+++ karma.conf.js
@@ -67,6 +67,12 @@ module.exports = function (config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
